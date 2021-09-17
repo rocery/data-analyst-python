c0a8accd55d3aaf3a51a7b896820de37b60db154
Sastra Nur Alamsyah <sastranuralamsyah@gmail.com>
2021-09-17 14:18:25 +0700

    17-09-2021.1

--- Statistic using Python for Data Science/Statistic using Python for Data Science.py
+++ Statistic using Python for Data Science/Statistic using Python for Data Science.py
@@ -1,5 +1,7 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+from scipy import stats
 raw_data = pd.read_csv("https://storage.googleapis.com/dqlab-dataset/dataset_statistic.csv", sep=';')
 
 # mengambil hanya data untuk produk 'A'
@@ -62,3 +64,144 @@ print (raw_data.corr(method='kendall'))
  
 # mencari korelasi 'spearman' untuk tiap pasang variabel
 print (raw_data.corr(method='spearman'))
+
+# ------------------------Visualisasi-------------------------
+# Scatter
+plt.figure()
+# visualisasi diagram pencar untuk variabel 'Pendapatan' dan 'Total' menggunakan 'plot.scatter' dari pandas
+raw_data.plot.scatter(x='Pendapatan', y='Total')
+plt.title('plot.scatter dari pandas', size=14)
+plt.tight_layout()
+plt.show()
+
+# visualisasi diagram pencar untuk variabel 'Pendapatan' dan 'Total' menggunakan 'plt.scatter' dari matplotlib
+plt.scatter(x='Pendapatan', y='Total', data=raw_data)
+plt.title('plt.scatter dari matplotlib', size=14)
+plt.tight_layout()
+plt.show()
+
+# Histogram
+plt.figure()
+# melihat distribusi data kolom 'Pendapatan' menggunakan 'hist' dari pandas
+raw_data.hist(column='Pendapatan')
+plt.title('.hist dari pandas', size=14)
+plt.tight_layout()
+plt.show()
+
+plt.figure()
+# melihat distribusi data kolom 'Pendapatan' menggunakan 'pyplot.hist' dari matplotlib.pyplot
+plt.hist(x='Pendapatan', data=raw_data)
+plt.xlabel('Pendapatan')
+plt.title('pyplot.hist dari matplotlib.pyplot', size=14)
+plt.tight_layout()
+plt.show()
+
+# Boxplot
+# melihat box plot dari kolom 'Pendapatan' menggunakan method '.boxplot' dari pandas
+plt.figure()
+raw_data.boxplot(column='Pendapatan')
+plt.title('.boxplot dari pandas', size=14)
+plt.tight_layout()
+plt.show()
+
+# melihat box plot dari kolom 'Pendapatan' menggunakan method '.boxplot' dari matplotlib
+plt.figure()
+plt.boxplot(x = 'Pendapatan', data=raw_data)
+plt.xlabel('Pendapatan')
+plt.title('pyplot.boxplot dari matplotlib.pyplot', size=14)
+plt.tight_layout()
+plt.show()
+
+# Bar plot
+# hitung frekuensi dari masing-masing nilai pada kolom 'Produk'
+class_freq = raw_data['Produk'].value_counts()
+
+# lihat nilai dari class_freq
+print(class_freq)
+
+plt.figure()
+# membuat bar plot dengan method `plot.bar()` dari pandas
+class_freq.plot.bar()
+plt.title('.bar dari pandas', size=14)
+plt.tight_layout()
+plt.show()
+
+plt.figure()
+# membuat bar plot dengan method `plt.bar()` dari matplotlib
+plt.bar(x=class_freq.index, height=class_freq.values)
+plt.title('plt.bar dari matplotlib.pyplot', size=14)
+plt.tight_layout()
+plt.show()
+
+# Pie chart
+plt.figure()
+# membuat pie chart menggunakan method 'pyplot.pie()' dari matplotlib
+plt.pie(class_freq.values, labels=class_freq.index)
+plt.title('plt.pie dari matplotlib.pyplot', size=14)
+plt.tight_layout()
+plt.show()
+
+plt.figure()
+# membuat pie chart menggunakan method 'plot.pie' dari pandas
+class_freq.plot.pie()
+plt.title('plot.pie dari pandas', size=14)
+plt.tight_layout()
+plt.show()
+
+# ------------------------Transformasi-------------------------
+plt.figure()
+raw_data.hist()
+plt.title('Histogram seluruh kolom', size=14)
+plt.tight_layout()
+plt.show()
+
+plt.figure()
+raw_data['Pendapatan'].hist()
+plt.title('Histogram pendapatan', size=14)
+plt.tight_layout()
+plt.show()
+
+plt.figure()
+# transformasi menggunakan akar lima
+np.power(raw_data['Pendapatan'], 1/5).hist()
+plt.title('Histogram pendapatan - ransformasi menggunakan akar lima', size=14)
+plt.tight_layout()
+plt.show()
+
+# simpan hasil transformasi
+pendapatan_akar_lima = np.power(raw_data['Pendapatan'], 1/5)
+
+plt.figure()
+# membuat qqplot pendapatan - transformasi menggunakan akar lima
+stats.probplot(pendapatan_akar_lima, plot=plt)
+plt.title('qqplot pendapatan - transformasi menggunakan akar lima', size=14)
+plt.tight_layout()
+plt.show()
+
+plt.figure()
+# membuat qqplot pendapatan
+stats.probplot(raw_data['Pendapatan'], plot=plt)
+plt.title('qqplot pendapatan', size=14)
+plt.tight_layout()
+plt.show()
+
+# Transformasi Box-Cox
+hasil, _ = stats.boxcox(raw_data['Pendapatan'])
+
+plt.figure()
+# Histogram
+plt.hist(hasil)
+plt.title('Histogram', size=14)
+plt.tight_layout()
+plt.show()
+
+plt.figure()
+# QQPlot
+stats.probplot(hasil, plot=plt)
+plt.title('qqplot', size=14)
+plt.tight_layout()
+plt.show()
+
+# Transformasi Data Kategorik ke Dalam Angka
+data_dummy_produk = pd.get_dummies(raw_data['Produk'])
+print(data_dummy_produk)
